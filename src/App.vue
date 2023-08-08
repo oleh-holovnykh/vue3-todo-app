893339526006044b61ecd74a4036e0c1b2575025
oleh <shankar.oleh@gmail.com>
2023-08-08 16:26:56 +0300

    added animation on todo hide/show

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,19 @@ export default {
   computed: {
     activeTodos() {
       return this.todos.filter(todo => !todo.completed)
+    },
+    completedTodos() {
+      return this.todos.filter(todo => todo.completed)
+    },
+    visibleTodos() {
+      switch (this.status) {
+        case 'active':
+          return this.activeTodos;
+        case 'completed':
+          return this.completedTodos;
+        default:
+          return this.todos;
+      }
     }
   },
   watch: {
@@ -70,16 +83,20 @@ export default {
         </form>
       </header>
 
-      <section class="todoapp__main">
+      <TransitionGroup
+        name="list"
+        tag="section"
+        class="todoapp__main"
+      >
         <TodoItem
-          v-for="(todo, index) of todos"
+          v-for="todo of visibleTodos"
           :key="todo.id"
           :todo="todo"
-          @update="todos[index] = $event"
-          @delete="todos.splice(index, 1)"
+          @update="Object.assign(todo, $event)"
+          @delete="todos.splice(todos.indexOf(todo), 1)"
         />
 
-      </section>
+      </TransitionGroup>
 
       <footer class="todoapp__footer">
         <span class="todo-count"> {{ activeTodos.length }} items left </span>
@@ -110,4 +127,16 @@ export default {
   </div>
 </template>
 <style>
+.list-enter-active,
+.list-leave-active {
+  max-height: 60px;
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  max-height: 0;
+  transform: scaleY(0);
+}
 </style>
